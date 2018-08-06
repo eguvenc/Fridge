74705be06fdcd3fc7fa92534ddf67ec22707bf3b
Ersin Guvenc <eguvenc@gmail.com>
2018-08-06 09:02:51 +0300

    Update FridgeInterface.php

--- src/FridgeInterface.php
+++ src/FridgeInterface.php
@@ -7,4 +7,19 @@
  */
 interface FridgeInterface
 {
+     /**
+     * Put drink(s) to fridge
+     * 
+     * @param  array  $drinks drinks
+     * @return void
+     */
+    public function put(array $drinks);
+
+    /**
+     * Returns to array of drink object(s)
+     * 
+     * @param  int    $requestedNumber number of drinks
+     * @return array
+     */
+    public function get(int $requestedNumber) : array;
 }
